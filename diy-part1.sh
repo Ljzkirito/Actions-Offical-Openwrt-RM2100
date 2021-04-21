9d88a35092db44bb29144f1a97cacfd0a3927367
Ljzkirito <Kirito.ljz@outlook.com>
2021-04-21 10:57:45 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -36,8 +36,26 @@ popd
 # Add luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/diy/luci-theme-argon
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/diy/luci-app-argon-config
-# Change dnsmasq to dnsmasq-full
-sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 # 使用官方ppp 2.4.9
 rm -rf package/network/services/ppp
 svn co https://github.com/Ljzkirito/openwrt-packages/trunk/ppp package/network/services/ppp
+# 更改时区
+sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+# 更改默认NTP服务器
+sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
+# version.mk: use tencent's opkg mirror
+sed -i 's/downloads.openwrt.org/mirrors.cloud.tencent.com/g' include/version.mk
+sed -i 's/downloads.openwrt.org/mirrors.cloud.tencent.com/g' package/base-files/image-config.in
+# Change dnsmasq to dnsmasq-full
+sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+# target.mk: use libustream-openssl instead of libustream-wolfssl
+#sed -i 's/libustream-wolfssl/libustream-openssl/g' include/target.mk
+# base-files: upgrade: use zcat command provided by busybox
+sed -i 's/"zcat/"busybox zcat/g' package/base-files/files/lib/upgrade/common.sh
+# toolchain: optimize for performance instead of size
+sed -i 's/CPU_CFLAGS = -Os -pipe/CPU_CFLAGS = -O2 -pipe/g' include/target.mk
+# kernel: set default nf_conntrack_max to 65536
+sed -i 's/nf_conntrack_max=16384/nf_conntrack_max=65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,12 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-# 更改时区
-sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
-sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
-sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
-sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
 # 修改 argon 为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # Custom miniupnp
